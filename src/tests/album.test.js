694e183c13d2a270e7803453889374dd58ccd80a
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-11 20:50:35 -0300

    advances

--- src/tests/album.test.js
+++ src/tests/album.test.js
@@ -5,6 +5,7 @@ require("../models")
 
 let album
 let artist
+let albumId
 
 beforeAll(async () => {
 
@@ -30,7 +31,7 @@ test("POST -> 'URL_ALBUMS', should return status code 201, and res.body.name ===
   const res = await request(app)
     .post(URL_ALBUMS)
     .send(album)
-
+  albumId = res.body.id
   expect(res.status).toBe(201)
   expect(res.body).toBeDefined()
   expect(res.body.name).toBe(album.name)
@@ -46,5 +47,29 @@ test("GET -> 'URL_ALBUMS', should return status code 200, and res.body.length ==
   expect(res.body).toBeDefined()
   expect(res.body).toHaveLength(1)
 
+})
+
+test("PUT -> '/api/v1/albums'/:id', should return status code 200, and res.body.firstName === albumUpdate.name", async () => {
+  const albumUpdate = {
+    name: "albumUpdate"
+  }
+
+  const res = await request(app)
+    .put(`/api/v1/albums/${albumId}`)
+    .send(albumUpdate)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.name).toBe(albumUpdate.name)
+})
+
+
+test("Delete -> '/api/v1/albums'/:id', should return status code 204", async () => {
+
+  const res = await request(app)
+    .delete(`/api/v1/albums/${albumId}`)
+
+  expect(res.status).toBe(204)
   await artist.destroy()
 })
+
